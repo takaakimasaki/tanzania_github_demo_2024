8765bfcea4631922e8f5d143c09c28c0b1f73b5f
Takaaki Masaki <takaakimasaki@users.noreply.github.com>
2024-04-30 09:20:14 +0200

    Added population statistics

--- code/demo.R
+++ code/demo.R
@@ -8,3 +8,8 @@ dat = WDI::WDI(indicator=c('NY.GDP.PCAP.KD'), start=2000, end=2021)
 dat %>%
   filter(country=="Tanzania" & !is.na(NY.GDP.PCAP.KD))%>%
   write_csv(here::here("data","GDPpc_constant_tza.csv"))
+
+dat_pop = WDI::WDI(indicator=c('SP.POP.TOTL'), start=2000, end=2021)
+dat_pop %>%
+  filter(country=="Tanzania" & !is.na(SP.POP.TOTL))%>%
+  write_csv(here::here("data","pop_tza.csv"))
